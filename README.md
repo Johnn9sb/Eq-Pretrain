e52f635af2a7f722f39bae550850e65ebd209490
j0hnng <johnn9sb@gmail.com>
2023-11-26 14:58:27 +0800

    2023.11.26-01

---
+++ README.md
@@ -0,0 +1,17 @@
+# Earthquake-Pretraining
+Earthquake Waveform Pre-trained Model
+PyTorch implementation of "Earthquake Waveform Pre-trained Model" with Fairseq.
+
+This implementation is currently designed for inference purposes only and is not intended for training.
+Please note that the current version supports inference functionalities exclusively.
+
+## Requirements and Installation
++ [PyTorch](https://pytorch.org/) version >= 1.10.0
++ Python version >= 3.8
++ To install fairseq and develop this implementation locally:
+```
+git clone https://github.com/j0hnng/Eq-Pretrain.git
+cd Eq-Pretrain
+cd fairseq-main
+pip install --editable ./
+```
---
+++ fairseq_cli/load_model.py
@@ -0,0 +1,23 @@
+import os
+import hydra
+from fairseq.dataclass.configs import FairseqConfig
+from fairseq.dataclass.initialize import hydra_init
+from fairseq import tasks
+
+@hydra.main(config_path='/work/u3601026/fairseq-main/fairseq/config', config_name="config")
+def main(cfg: FairseqConfig) -> float:
+    
+    # Load Model for EQ-Pretrain
+    hydra_init(cfg)
+    task = tasks.setup_task(cfg.task)
+    model_w2v = task.build_model(cfg.model)
+    print(model_w2v)
+
+    # Load Pre-trained Weights
+    # Checkpoint = torch.load(your_model_path)
+    # model.load_state_dict(Checkpoint["model"], strict=True)
+
+    # Please run and edit your scripts within this block
+
+if __name__ == "__main__":
+    main()
