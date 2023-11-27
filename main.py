d30e5019f924c0b23f74264e26e89b771ec3f3f5
skysora <sora910321@gmail.com>
2023-11-27 10:06:10 +0000

    change folder struct

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+from model.EQ_Pretrain.load_model import load_fairseq
+
+
+
+if __name__ == "__main__":
+    model_w2v = load_fairseq('./model/EQ_Pretrain/config',"eq_pretrain")
---
+++ setup.bash
@@ -0,0 +1,9 @@
+pip install --editable ./model/fairseq
+
+
+
+python load_model.py --config-dir ./config --config-name eq_pretrain
+
+
+
+python load_model.py --config-dir ./model/EQ_Pretrain/config --config-name eq_pretrain
