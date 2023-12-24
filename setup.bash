58bed7916d7ec63aec6e6d9f12fc10f787cecdde
j0hnng <johnn9sb@gmail.com>
2023-12-25 04:15:55 +0800

    setup update

--- setup.bash
+++ setup.bash
@@ -1,9 +1,5 @@
+pip install -r requirements.txt
+
 pip install --editable ./model/fairseq
 
-
-
-python load_model.py --config-dir ./config --config-name eq_pretrain
-
-
-
-python load_model.py --config-dir ./model/EQ_Pretrain/config --config-name eq_pretrain
+python load_model.py 
