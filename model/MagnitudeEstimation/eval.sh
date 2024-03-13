d9ee5ac854dac396aa19d4ae563dee794b564742
Johnn9sb <johnn9sb@gmail.com>
2024-03-13 22:43:22 +0800

    eval.sh

---
+++ model/MagnitudeEstimation/eval.sh
@@ -0,0 +1,10 @@
+CUDA_VISIBLE_DEVICES=1 \
+python test.py \
+--save_path '/mnt/nas3/johnn9/mag_checkpoint/lem_freeze_50000' \
+--model_opt 'w2v' \
+--decoder_type 'cnn' \
+--batch_size 64 \
+--workers 4 \
+--level 4 \
+--without_noise 'False' \
+--w2v_path '/mnt/nas3/johnn9/pretrain/11-54-34/checkpoints/checkpoint_46_50000.pt' \
--- model/MagnitudeEstimation/train.sh
+++ model/MagnitudeEstimation/train.sh
@@ -9,15 +9,5 @@ python train.py \
 --workers 4 \
 --level 4 \
 --w2v_path '/mnt/nas3/johnn9/pretrain/11-54-34/checkpoints/checkpoint_46_50000.pt' \
-# --without_noise False \
+--without_noise 'False' \
 
-# CUDA_VISIBLE_DEVICES=1 \
-# python test.py \
-# --save_path '/mnt/nas3/johnn9/mag_checkpoint/lem_freeze_50000' \
-# --model_opt 'w2v' \
-# --decoder_type 'cnn' \
-# --batch_size 32 \
-# --workers 4 \
-# --level 4 \
-# --without_noise 'False' \
-# --w2v_path '/mnt/nas3/johnn9/pretrain/11-54-34/checkpoints/checkpoint_46_50000.pt' \
